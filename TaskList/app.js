70d86c6e7ef4f4e9b6e11538dfac73e5980972fd
= <parshvashah414@gmail.com>
2019-09-18 11:32:43 -0400

    Finished UI and begun js construction

---
+++ TaskList/app.js
@@ -0,0 +1,7 @@
+// Define UI Variables
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelectorAll('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = documet.querySelector('#task');
